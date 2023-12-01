2edcbb84155bcb327bde2f8dbbfc3cf14693bacb
root <paulorubebe@yahoo.com>
2023-12-01 16:22:42 +0100

    Refractor code

--- src/lib.rs
+++ src/lib.rs
@@ -85,15 +85,15 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     // results
 
     contents.lines()
-        .filter(|line| line.trim().contains(query))
-        .map(|line| line.trim()).collect()
+        .filter(|line| line.contains(query))
+        .map(|line| line).collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
      contents.lines()
-        .filter(|line| line.to_lowercase().trim().contains(&query))
-        .map(|line| line.trim()).collect()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .map(|line| line).collect()
 }
 
 #[cfg(test)]
@@ -104,10 +104,10 @@ mod tests {
     fn case_sensitive() {
         let query = "duct";
         let contents  = "\
-        Rust:
-        safe, fast, productive.
-        Pick three.
-        Duct tape.";
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
 
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
@@ -117,10 +117,10 @@ mod tests {
     fn case_insensitive() {
          let query = "rUsT";
         let contents  = "\
-        Rust:
-        safe, fast, productive.
-        Pick three.
-        Trust me.";
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
 
         assert_eq!(vec!["Rust:", "Trust me."], search_case_insensitive(query, contents));
     }
